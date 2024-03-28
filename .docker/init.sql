a449ef3ddc54e796467091e635a8494729716399
Salazar <lsalazar@ciasc.sc.gov.br>
2024-03-28 15:42:16 -0300

    Ajuste dos arquivos docker

--- .docker/init.sql
+++ .docker/init.sql
@@ -1,9 +1,9 @@
 -- Install the extension we just compiled
 CREATE EXTENSION IF NOT EXISTS vector;
 
-CREATE TABLE IF NOT EXISTS embeddings (
+CREATE TABLE IF NOT EXISTS embeddings_placas (
   id bigserial PRIMARY KEY,
-  embedding vector(512),
+  embedding vector(1536),
   content TEXT,
   created_at timestamptz DEFAULT now()
 );
